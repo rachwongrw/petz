5bf5869362397204df630fa7cd9b3a55d834cf45
rachwongrw <rwrachelwong@gmail.com>
2021-02-05 10:05:11 -0800

    code refactor

--- App.js
+++ App.js
@@ -1,18 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import PetDetail from './src/components/PetDetail';
+import Pet from './src/components/PetDetail';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>G'Day</Text>
-      <PetDetail name="Koda" img={require('./assets/koda.jpg')}/>
-      <PetDetail name="Murphy" img={require('./assets/murphy.jpg')}/>
-      <PetDetail name="Effie" img={require('./assets/effie.jpg')}/>
+      {
+        pets.map(p => <Pet name={p.name} img={p.img}/>)
+      }
     </View>
   );
 }
+
+const pets = [
+  {name: "Koda", img: require('./assets/koda.jpg')},
+  {name: "Murphy", img: require('./assets/murphy.jpg')},
+  {name: "Effie", img: require('./assets/effie.jpg')}
+]
 
 const styles = StyleSheet.create({
   container: {
