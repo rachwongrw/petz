7b548b784d41c096b78735e1cfbdded966393cf3
rachwongrw <rwrachelwong@gmail.com>
2021-02-05 09:46:13 -0800

    add src/components folder

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import PetDetail from './src/components/PetDetail';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>G'Day</Text>
-      <StatusBar style="auto" />
+      <PetDetail name="Koda"/>
+      <PetDetail name="Murphy"/>
+      <PetDetail name="Effie"/>
     </View>
   );
 }
---
+++ src/components/PetDetail.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { StyleSheet, Text } from 'react-native'
+
+const PetDetail = (props) => {
+  return <Text>{props.name}</Text>
+}
+
+const styles = StyleSheet.create({})
+
+export default PetDetail
